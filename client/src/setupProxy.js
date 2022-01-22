e223574bf6ce5eed395df5467c8ba2093f5077f7
JODAHYE <57217119+JODAHYE@users.noreply.github.com>
2022-01-23 00:36:28 +0900

    modified

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,8 +3,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
     createProxyMiddleware('/api', {
-      // target: 'https://colorful-memo.herokuapp.com',
-      target: 'http://localhost:5000',  //node 서버
+      target: 'https://colorful-memo.herokuapp.com',
+      // target: 'http://localhost:5000',  //node 서버
       changeOrigin: true,
     })
   );

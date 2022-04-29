08ced03f3613bdf0d5bb4738b90f8cae5c23dcd0
JODAHYE <57217119+JODAHYE@users.noreply.github.com>
2022-04-29 16:13:38 +0900

    fix : require을 import로 수정

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import user from "./routes/user.js";
 import post from "./routes/post.js";
 import category from "./routes/category.js";
 import cors from "cors";
+import path from "path";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -21,7 +22,6 @@ app.use("/category", category);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build")); // set static folder
   //returning frontend for any route other than api
-  const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
